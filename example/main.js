ef807793943fba8c1c0a6f66244dd09535e7ce1b
Nghia Tran <vinhnghiatran@gmail.com>
2017-03-10 12:38:20 +0700

    Removed unnecessary files

--- example/main.js
+++ example/main.js
@@ -33,16 +33,13 @@ import MapboxGLMap from 'react-map-gl';
 import {FPSStats} from 'react-stats';
 import LayerSelector from './layer-selector';
 import LayerInfo from './layer-info';
-
 import * as request from 'd3-request';
-import LAYER_CATEGORIES, {DEFAULT_ACTIVE_LAYERS} from './layer-examples';
 
 import DeckGL from '../src/react/deckgl';
 import {ReflectionEffect} from '../src/experimental';
 
 import {updateMap, loadChoropleths, loadExtrudedChoropleths, loadHexagons,
 loadPoints, swapData} from './action'
-import { processHexagons, pointsToArcs, pointsToLines} from './helper'
 import { reducer } from './reducer'
 
 // ---- Default Settings ---- //
@@ -60,7 +57,6 @@ class ExampleApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeExamples: DEFAULT_ACTIVE_LAYERS,
       selectedHexagons: [],
       hoverHexagon: null,
       hoverPoint: null,
@@ -75,24 +71,11 @@ class ExampleApp extends React.Component {
   componentWillMount() {
     this._handleResize();
     window.addEventListener('resize', this._handleResize);
-
-    this._loadJsonFile(CHOROPLETHS_FILE, this._handleChoroplethsLoaded);
-    this._loadJsonFile(EXTRUDED_CHOROPLETHS_FILE, this._handleExtrudedChoroplethsLoaded);
-    this._loadCsvFile(HEXAGONS_FILE, this._handleHexagonsLoaded);
     this._loadCsvFile(POINTS_FILE, this._handlePointsLoaded);
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this._handleResize);
-  }
-
-  _loadJsonFile(path, onDataLoaded) {
-    request.json(path, function loadJson(error, data) {
-      if (error) {
-        console.error(error);
-      }
-      onDataLoaded(data);
-    });
   }
 
   _loadCsvFile(path, onDataLoaded) {
@@ -116,14 +99,6 @@ class ExampleApp extends React.Component {
     this.props.dispatch(loadPoints(data));
   }
 
-  @autobind _handleChoroplethsLoaded(data) {
-    this.props.dispatch(loadChoropleths(data));
-  }
-
-  @autobind _handleExtrudedChoroplethsLoaded(data) {
-    this.props.dispatch(loadExtrudedChoropleths(data));
-  }
-
   @autobind _handleResize() {
     this.setState({width: window.innerWidth, height: window.innerHeight});
   }
@@ -135,128 +110,21 @@ class ExampleApp extends React.Component {
     this.props.dispatch(updateMap(mapViewState));
   }
 
-  @autobind _handleChoroplethHovered(info) {
-    info.type = 'choropleth';
-    this.setState({hoverChoropleth: info});
-  }
-
-  @autobind _handleChoroplethClicked(info) {
-    info.type = 'choropleth';
-    this.setState({clickItem: info});
-  }
-
-  @autobind _handleHexagonHovered(info) {
-    info.type = 'hexagon';
-
-    const {hexData} = this.props;
-    let selectedHexagons = [];
-    if (info.index >= 0) {
-      selectedHexagons = [{
-        ...hexData[info.index],
-        color: [0, 0, 255]
-      }];
-    }
-
-    this.setState({
-      hoverHexagon: info,
-      selectedHexagons
-    });
-  }
-
-  @autobind _handleHexagonClicked(info) {
-    info.type = 'hexagon';
-    this.setState({clickItem: info});
-    this.props.dispatch(swapData());
-  }
-
-  @autobind _handleScatterplotHovered(info) {
-    info.type = 'point';
-    this.setState({hoverPoint: info});
-  }
-
-  @autobind _handleScatterplotClicked(info) {
-    info.type = 'point';
-    this.setState({clickItem: info});
-  }
-
-  @autobind _handleArcHovered(info) {
-    info.type = 'arc';
-    this.setState({hoverArc: info});
-  }
-
-  @autobind _handleArcClicked(info) {
-    info.type = 'arc';
-    this.setState({clickItem: info});
-  }
-
-  @autobind _handleLineHovered(info) {
-    info.type = 'line';
-    this.setState({hoverLine: info});
-  }
-
-  @autobind _handleLineClicked(info) {
-    info.type = 'line';
-    this.setState({clickItem: info});
-  }
-
-  @autobind _onChangeLayers(exampleName) {
-    const {activeExamples} = this.state;
-    activeExamples[exampleName] = !activeExamples[exampleName];
-    this.setState({activeExamples});
-  }
-
   @autobind _onWebGLInitialized(gl) {
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
   }
 
   _renderExamples() {
-    const props = {
-      ...this.props,
-      ...this.state,
-      onChoroplethHovered: this._handleChoroplethHovered,
-      onChoroplethClicked: this._handleChoroplethClicked,
-      onHexagonHovered: this._handleHexagonHovered,
-      onHexagonClicked: this._handleHexagonClicked,
-      onScatterplotHovered: this._handleScatterplotHovered,
-      onScatterplotClicked: this._handleScatterplotClicked,
-      onArcHovered: this._handleArcHovered,
-      onArcClicked: this._handleArcClicked,
-      onLineHovered: this._handleLineHovered,
-      onLineClicked: this._handleLineClicked
-    };
-    const layers = [];
-    for (const categoryName of Object.keys(LAYER_CATEGORIES)) {
-      for (const exampleName of Object.keys(LAYER_CATEGORIES[categoryName])) {
-        if (this.state.activeExamples[exampleName]) {
-          // An example can be a function returning a deck.gl layer instance
-          // or an array of such a function and a prop generating function
-          let example = LAYER_CATEGORIES[categoryName][exampleName];
-          let layerProps = props;
-          /* eslint-disable max-depth */
-          if (Array.isArray(example)) {
-            const makeProps = example[1];
-            example = example[0];
-            layerProps = {
-              ...props,
-              ...makeProps(),
-              id: exampleName
-            };
-          }
-          /* eslint-enable max-depth */
-          layers.push(example(layerProps));
-        }
-      }
-    }
-    return layers;
+    return [];
   }
 
   _renderOverlay() {
-    const {choropleths, extrudedChoropleths, hexagons, points, mapViewState} = this.props;
+    const {points, mapViewState} = this.props;
     const {width, height} = this.state;
 
     // wait until data is ready before rendering
-    if (!extrudedChoropleths || !choropleths || !points || !hexagons) {
+    if (!points ) {
       return [];
     }
 
@@ -296,9 +164,6 @@ class ExampleApp extends React.Component {
     return (
       <div>
         { this._renderMap() }
-        <LayerSelector { ...this.state }
-          examples={LAYER_CATEGORIES}
-          onChange={this._onChangeLayers}/>
         <LayerInfo { ...this.state }/>
       </div>
     );
