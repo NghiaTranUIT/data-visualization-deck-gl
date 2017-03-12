be9f296e7d50cb890c736b44ec7f719d94e68395
Nghia Tran <vinhnghiatran@gmail.com>
2017-03-12 11:35:30 +0700

    Fixed name data. remove code

--- example/constants.js
+++ example/constants.js
@@ -1,3 +1,4 @@
+
 export const MapMode = {
   NONE:             'NONE',
   TREES:            'TREES',
@@ -8,8 +9,10 @@ export const MapMode = {
 
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibmdoaWF0cmFuIiwiYSI6ImNpenhxcjBsejAxc2EycXFycTAzbjBqMHYifQ.lrdb9bCOiTpOjcO254IQBw'
 
-export const SMALL_FLIGHT_DATA = './example/data/very-small-chuck-flight-data.csv'
+export const SMALL_FLIGHT_DATA = './example/data/flight-record-tiny.csv'
+
+export const MEDIUM_FLIGHT_DATA = './example/data/flight-record-medium.csv'
 
 export const AIRPORT_DATA = './example/data/airports.csv'
 
-export const TREE_DATA = './example/data/tree_census.csv'
+export const TREE_DATA = './example/data/tree_new_york.csv'
